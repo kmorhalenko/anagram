82a6d16a52e00b6857251f2e9adae0250e70c543
Kyrylo Morhalenko <k.morhalenko@aimprosoft.com>
2019-03-25 15:36:00 +0200

    first test added

--- src/test/java/com/kirll/morhalenko/MainTest.java
+++ src/test/java/com/kirll/morhalenko/MainTest.java
@@ -5,32 +5,53 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class MainTest {
+    private AnagramMaker anagramMaker = new AnagramMaker();
 
-    private AnagramMaker anagramMaker;
-//
-//    @BeforeClass
-//    public static void beforeClass() {
-//        System.out.println("Before CalculatorTest.class");
-//    }
-//
-//    @AfterClass
-//    public  static void afterClass() {
-//        System.out.println("After CalculatorTest.class");
-//    }
-
-    @Before
-    public void initTest() {
-        anagramMaker = new AnagramMaker();
-    }
-
-    @After
-    public void afterTest() {
-        anagramMaker = null;
+    @BeforeClass
+    public static void beforeClass() {
+        System.out.println(" _____ _____ ____ _____   ____ _____  _    ____ _____ _____ ____ ");
+        System.out.println("|_   _| ____/ ___|_   _| / ___|_   _|/ \\  |  _ \\_   _| ____|  _ \\");
+        System.out.println("  | | |  _| \\___ \\ | |   \\___ \\ | | / _ \\ | |_) || | |  _| | | | |");
+        System.out.println("  | | | |___ ___) || |    ___) || |/ ___ \\|  _ < | | | |___| |_| |");
+        System.out.println("  |_| |_____|____/ |_|   |____/ |_/_/   \\_\\_| \\_\\|_| |_____|____/ \n");
     }
 
     @Test
-    public void reverse() throws Exception {
-        assertEquals("ewq123", anagramMaker.splitAndReverseString("qwe123"));
+    public void reverseNumericalString() throws Exception {
+        assertEquals("1234567890", anagramMaker.splitAndReverseString("1234567890"));
+    }
+
+    @Test
+    public void specialCharactersNotReverse() throws Exception {
+        assertEquals("!@#$%^&*(()", anagramMaker.splitAndReverseString("!@#$%^&*(()"));
+
+    }
+
+    @Test
+    public void reverseStringOfCharacters() throws Exception {
+        assertEquals("privetMir", anagramMaker.splitAndReverseString("riMtevirp"));
+
+    }
+
+    @Test
+    public void splitAndReverseString() throws Exception {
+        assertEquals("qwerty asdfgh zxcvb", anagramMaker.splitAndReverseString("ytrewq hgfdsa bvcxz"));
+    }
+
+    @Test
+    public void reverseEmptyString() {
+        assertEquals("", anagramMaker.splitAndReverseString("      "));
+    }
+
+    @Test
+    public void reverseStringWithCharactersAndNumbers() {
+        assertEquals("qwe123qwe", anagramMaker.splitAndReverseString("ewq123ewq"));
+    }
+
+    @Test
+    public void reverseStringWithCharactersAndSpecialCharacters() {
+        assertEquals("sad!@#qwe", anagramMaker.splitAndReverseString("ewq!@#das"));
     }
 
 }
+
